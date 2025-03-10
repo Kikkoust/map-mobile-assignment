3516cc0b2fe37f33edb34f62a003695e566b3b23
Kikkoust <156084871+Kikkoust@users.noreply.github.com>
2025-03-10 18:29:30 +0200

    Button and "Name", "Description" TextInputs added to AddLocations.js

--- screens/AddLocation.js
+++ screens/AddLocation.js
@@ -1,15 +1,51 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Pressable, TextInput } from 'react-native';
 
-export default function AddLocationScreen() {
+export default function AddLocation({ navigation }) {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Add location</Text>
+    <View>
+
+      <TextInput
+        value={name}
+        onChangeText={setName}
+        placeholder="Name"
+        keyboardType="default"
+      />
+
+      <TextInput
+        value={description}
+        onChangeText={setDescription}
+        placeholder="Description"
+        keyboardType="default"
+      />
+
+      <Pressable style={addLocStyle.button} onPress={() => navigation.navigate("Locations")}>
+        <Text style={addLocStyle.text}>Add new location</Text>
+      </Pressable>
+
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
-  text: { fontSize: 20, fontWeight: 'bold' },
+const addLocStyle = StyleSheet.create({
+  button: {  
+  alignItems: 'center',
+  fontWeight: 'bold',
+  backgroundColor: 'purple',
+  borderRadius: 18,
+  padding: 10,
+  margin: 10,
+
+  },
+
+  text:{
+    fontWeight:'bold',
+    color: 'white',
+  },
+
+
 });
--- screens/LocationsScreen.js
+++ screens/LocationsScreen.js
@@ -4,21 +4,24 @@ import { View, Text, StyleSheet, Pressable } from 'react-native';
 export default function LocationScreen({ navigation }) {
   return (
     <View>
-        <Pressable style={addLocStyle.button} onPress={() => navigation.navigate("Add location")}>
-          <Text style={addLocStyle.text}>Add new location</Text>
-        </Pressable>
+      <Pressable style={addLocStyle.button} onPress={() => navigation.navigate("Add location")}>
+        <Text style={addLocStyle.text}>Add new location</Text>
+      </Pressable>
 
-        
+      
+
     </View>
   );
 }
 
+
+/*ADD NEW BUTTON STYLES*/
 const addLocStyle = StyleSheet.create({
   button: {  
   alignItems: 'center',
   fontWeight: 'bold',
   backgroundColor: 'purple',
-  borderRadius: 6,
+  borderRadius: 18,
   padding: 10,
   margin: 10,
 
