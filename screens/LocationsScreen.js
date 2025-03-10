4d8d6d062b482e7fdd4dea998e02b2f98ee23277
Kikkoust <156084871+Kikkoust@users.noreply.github.com>
2025-03-10 18:16:12 +0200

    "Add new location"-button added and some styling.

--- screens/LocationsScreen.js
+++ screens/LocationsScreen.js
@@ -1,15 +1,34 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Pressable } from 'react-native';
 
-export default function LocationScreen() {
+export default function LocationScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Locations</Text>
+    <View>
+        <Pressable style={addLocStyle.button} onPress={() => navigation.navigate("Add location")}>
+          <Text style={addLocStyle.text}>Add new location</Text>
+        </Pressable>
+
+        
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
-  text: { fontSize: 20, fontWeight: 'bold' },
+const addLocStyle = StyleSheet.create({
+  button: {  
+  alignItems: 'center',
+  fontWeight: 'bold',
+  backgroundColor: 'purple',
+  borderRadius: 6,
+  padding: 10,
+  margin: 10,
+
+  },
+
+  text:{
+    fontWeight:'bold',
+    color: 'white',
+  },
+
+
 });
+
